8e6bfbbbc0251e661f9f5823ddb41d49e8c10040
appleRui <apple31581@gmail.com>
2023-07-06 22:37:34 +0900

    fix

---
+++ i18n/ja.ts
@@ -0,0 +1,9 @@
+export const jaTranslate = {
+  ja: {
+    "Sign In": "ログイン",
+    "Sign in": "ログイン",
+    "Sign Out": "ログアウト",
+    "Create Account": "新規登録",
+    "Enter your email": "メールアドレスを入力してください",
+  }
+};
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,11 +4,20 @@ import type { AppProps } from "next/app";
 import awsExports from "../src/aws-exports";
 import "../styles/globals.css";
 import '@aws-amplify/ui-react/styles.css';
+import { I18n } from 'aws-amplify';
+import { translations } from '@aws-amplify/ui';
+import { jaTranslate } from "../i18n/ja";
 
+// Amplifyの設定
 Amplify.configure({ ...awsExports });
+I18n.putVocabularies(translations);
+I18n.putVocabularies(jaTranslate);
+I18n.setLanguage('ja');
 
 const App = ({ Component, pageProps }: AppProps) => {
   return (
+    // 認証用コンポーネント
+    // 認証がされていない場合、認証用コンポーネントが表示される
     <Authenticator.Provider>
       <Component {...pageProps} />
     </Authenticator.Provider>
